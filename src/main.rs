1e2b31eee2b049b3901ae7a7fd17b062f065415d
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-14 11:56:21 -1000

    proto 0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+
+use std::io;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let (mut read, mut write) = socket.split();
+            tokio::io::copy(&mut read, &mut write).await.unwrap();
+        });
+    }
 }
